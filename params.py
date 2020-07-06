0d0ec13e67ec42d31961b6fa53f6b5f804b7b95b
Vadim Saroka <vsaroka87@gmail.com>
2020-07-05 18:01:19 -0700

    first commit

---
+++ params.py
@@ -0,0 +1,45 @@
+HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0",
+           "Accept-Encoding": "gzip, deflate",
+           "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT": "1",
+           "Connection": "close", "Upgrade-Insecure-Requests": "1"}
+
+EMAIL = "test@gmail.com"
+PASSWORD = "password"
+
+EMAIL2 = "test2@gmail.com"
+
+
+"""
+URLS: copy/paste a complete URL from AMAZON that you want to track
+"""
+
+URLS = [
+    ["https://www.amazon.com/insta360-Adaptive-Stabilization-Waterproof-Control/dp/B083R4VMMC/ref=sr_1_3?dchild=1"
+     "&keywords=instaone+360+r&qid=1593813124&sr=8-3", 420],
+    ["https://www.amazon.com/GoPro-MAX-Bundle-256GB-Card/dp/B07Z7FWL1L/ref=sr_1_5?dchild=1&keywords=gopro+360+max&qid"
+     "=1593829597&sr=8-5", 500],
+    ["https://www.amazon.com/Valvoline-788699-Multi-Vehicle-Automatic-Transmission/dp/B072KNNZLV/ref=sr_1_2?dchild=1"
+     "&keywords=Valvoline+Import+Multi-Vehicle+%28ATF%29+Full+Synthetic+Automatic+Transmission+Fluid+1+QT&qid"
+     "=1593829924&sr=8-2", 5],
+    ["https://www.amazon.com/Almohadillas-repuesto-almohadillas-compatibles-auriculares/dp/B073Q1DCR6/ref=sr_1_20"
+     "?dchild=1&keywords=White+Replacement+Earpads%2C+AGPTEK+2+Pieces+Foam+Ear+Pad+Cushion+Compatible+with+Beats+Solo"
+     "+2+Wireless+Headphone&qid=1593829976&sr=8-20", 5],
+    ["https://www.amazon.com/Fleximounts-Monitor-Computer-Release-Capacity/dp/B0795DMGQ8/ref=sr_1_1?dchild=1&keywords"
+     "=Fleximounts+F8+Monitor+Mount+Desk+Stand+for+24+to+36+inches+Ultra+Wide+Computer+Monitor+with+Quick+Release"
+     "+Fast+Insert+LCD+Bracket%2C+Weight+Capacity+from+6.6+to+19.8+lbs+%28Single+arm%29&qid=1593830023&sr=8-1", 50],
+    ["https://www.amazon.com/TP-solar-Foldable-Portable-Generator-Smartphones/dp/B07ZZ8VY2V/ref=sr_1_1?dchild=1"
+     "&keywords=TP-solar+100W+Foldable+Solar+Panel+Charger+Kit+for+Portable+Generator+Power+Station+Smartphones"
+     "+Laptop+Car+Boat+RV+Trailer+12v+Battery+Charging+%28Dual+5V+USB+%26+19V+DC+Output%29&qid=1593830080&sr=8-1",
+     150],
+    ["https://www.amazon.com/Insta360-ONE-Twin-Action-Bundle/dp/B0872HGHRD/ref=sr_1_15?crid=K7MYX9NFENRD&dchild=1"
+     "&keywords=insta360+r+twin+edition&qid=1593843551&sprefix=insta360+r+tw%2Caps%2C465&sr=8-15",
+     450],
+    ["https://www.amazon.com/Insta360-Camera-Angle-SanDisk-Monopod/dp/B087KSG5HP/ref=sr_1_8?crid=K7MYX9NFENRD&dchild"
+     "=1&keywords=insta360+r+twin+edition&qid=1593843551&sprefix=insta360+r+tw%2Caps%2C465&sr=8-8",
+     450],
+    ["https://www.amazon.com/DJI-Mavic-Air-ActiveTrack-CP-MA-00000176/dp/B086X7QKHD/ref=pd_sbs_421_4/133-3498502"
+     "-3795000?_encoding=UTF8&pd_rd_i=B086X7QKHD&pd_rd_r=9f027fcd-1c55-4c5f-801d-9e3d387ec4f9&pd_rd_w=bbzci&pd_rd_wg"
+     "=kOiRC&pf_rd_p=bdc67ba8-ab69-42ee-b8d8-8f5336b36a83&pf_rd_r=EQQA5QM2G7JPN32ZRMXB&refRID=EQQA5QM2G7JPN32ZRMXB&th"
+     "=1",
+     700]
+]
---
+++ pytracker.py
@@ -0,0 +1,78 @@
+import datetime
+from bs4 import BeautifulSoup
+import requests
+from time import sleep
+import smtplib
+from params import HEADERS, URLS, EMAIL, PASSWORD, EMAIL2
+
+
+class Bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+def send_mail(url):
+    print(f"{Bcolors.WARNING}Trying to send an email{Bcolors.ENDC}")
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+    server.login(EMAIL, PASSWORD)
+    subject = "Price fell down!"
+    body = f"Check the amazon link {url}"
+    msg = f"Subject: {subject}\n\n{body}"
+
+    server.sendmail(
+        EMAIL,
+        EMAIL2,
+        msg
+    )
+    print(f"{Bcolors.OKGREEN}EMAIL HAS BEEN SENT!{Bcolors.ENDC}")
+    server.quit()
+
+
+def title_handler(text):
+    text = text.split(" ")
+    new_title = ""
+    for t in text:
+        if len(new_title) + len(t) < 40:
+            new_title = new_title + " " + t
+        else:
+            new_title = new_title + "..."
+            return new_title.strip()
+    return new_title.strip()
+
+
+def check():
+    for s in URLS:
+        page = requests.get(s[0], headers=HEADERS)
+        sleep(60)
+        soup = BeautifulSoup(page.content, "html.parser")
+        price = float(str(soup.find(id="price_inside_buybox").text).split("$")[1])
+        title = str(soup.find(id='productTitle').text).strip()
+        if price and title:
+            print(f"{Bcolors.HEADER}{title_handler(title)}{Bcolors.ENDC}\n"
+                  f"{Bcolors.OKBLUE}Date: {datetime.datetime.now()}{Bcolors.ENDC}"
+                  f"{Bcolors.OKBLUE}\nCurrent price: {Bcolors.BOLD}${price}{Bcolors.ENDC}{Bcolors.ENDC}"
+                  f"{Bcolors.OKBLUE}vs desirable price: {Bcolors.BOLD}${s[1]}{Bcolors.ENDC}{Bcolors.ENDC}"
+                  f"{Bcolors.OKBLUE} \n{Bcolors.UNDERLINE}{s[0]}{Bcolors.ENDC}{Bcolors.ENDC}\n")
+        if price <= s[1]:
+            send_mail(s[0])
+
+
+while True:
+    try:
+        check()
+    except AttributeError as e:
+        print(f"{Bcolors.FAIL}{e}{Bcolors.ENDC}")
+        print(f"{Bcolors.WARNING}Probably, you will need to reduce the number of requests to Amazon, "
+              f"in order not to be blocked as a bot.{Bcolors.ENDC}\n")
+        sleep(30)
+        continue
+    sleep(3600)
